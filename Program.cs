7570f9c83ab59c088360ce40a87e549cd3a5a5c2
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2023-10-22 14:39:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Lütfen bir metin ifadesi girin:");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(' ');
+
+        foreach (string word in words)
+        {
+            bool hasConsecutiveConsonants = CheckConsecutiveConsonants(word);
+            Console.Write(hasConsecutiveConsonants + " ");
+        }
+    }
+
+    static bool CheckConsecutiveConsonants(string word)
+    {
+        for (int i = 0; i < word.Length - 1; i++)
+        {
+            char currentChar = char.ToLower(word[i]);
+            char nextChar = char.ToLower(word[i + 1]);
+
+            if (IsConsonant(currentChar) && IsConsonant(nextChar))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static bool IsConsonant(char character)
+    {
+        char[] consonants = { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+        return Array.Exists(consonants, c => c == character);
+    }
+}
